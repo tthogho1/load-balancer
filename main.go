1e8f9087fffe90b83a04efdd393a0118e0aae91a
unknown <tthogho1@gmail.com>
2022-02-23 23:01:52 +0900

    send request for multi server

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
+	"sync"
+	//"net/http/httputil"
+	//"net/url"
 )
 
 var severCount = 0
@@ -17,16 +20,31 @@ const (
 	PORT    = "1338"
 )
 
+var SERVER_LIST []string = []string{SERVER1, SERVER2, SERVER3}
+var wg sync.WaitGroup
+var mutex = sync.Mutex{}
+
 // Serve a reverse proxy for a given url
-func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request) {
-	// parse the url
-	url, _ := url.Parse(target)
+func serveReverseProxy(target string, slice *[]string) {
 
-	// create the reverse proxy
-	proxy := httputil.NewSingleHostReverseProxy(url)
+	resp, err := http.Get(target)
 
-	// Note that ServeHttp is non blocking and uses a go routine under the hood
-	proxy.ServeHTTP(res, req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mutex.Lock()
+	*slice = append(*slice, string(body))
+	mutex.Unlock()
+
+	wg.Done()
 }
 
 // Log the typeform payload and redirect url
@@ -34,28 +52,32 @@ func logRequestPayload(proxyURL string) {
 	log.Printf("proxy_url: %s\n", proxyURL)
 }
 
-// Balance returns one of the servers based using round-robin algorithm
-func getProxyURL() string {
-	var servers = []string{SERVER1, SERVER2, SERVER3}
-
-	server := servers[severCount]
-	severCount++
-
-	// reset the counter and start from the beginning
-	if severCount >= len(servers) {
-		severCount = 0
-	}
-
-	return server
-}
-
 // Given a request send it to the appropriate url
 func handleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
-	url := getProxyURL()
 
-	logRequestPayload(url)
+	wg.Add(len(SERVER_LIST))
 
-	serveReverseProxy(url, res, req)
+	var slice []string
+
+	i := 0
+	for ; i < len(SERVER_LIST); i++ {
+		url := SERVER_LIST[i]
+		logRequestPayload(url)
+		go serveReverseProxy(url, &slice)
+	}
+
+	wg.Wait()
+	//
+	// change below code
+	//
+	i = 0
+	for ; i < len(slice); i++ {
+		println(slice[i])
+	}
+
+	//
+	io.WriteString(res, "Hello, world!\n")
+
 }
 
 func main() {
